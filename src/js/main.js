8eaa27789b709086c5c27782eb36910a21b95e96
Corentin <besqueutcorentin@gmail.com>
2024-02-19 16:58:28 +0100

    save

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,15 @@ let navbar=document.querySelector(".navbar");
 menu.onclick = () => {
     navbar.classList.toggle("open");
 }
+
+var windowHeight = window.innerHeight;
+window.addEventListener('scroll', function() {
+    var chaticon = document.querySelector('.chaticon');
+    chaticon.classList.add('scrolled');
+    var scrollTop = window.scrollY;
+    var documentHeight = document.body.scrollHeight;
+    if (scrollTop === 0 || (scrollTop + windowHeight) >= documentHeight) {
+        chaticon.classList.remove('scrolled');
+    }
+});
+
