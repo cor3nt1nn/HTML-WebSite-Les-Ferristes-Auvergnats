fd39e2c155875a420e5e215331cf45740ffd600e
Corentin <besqueutcorentin@gmail.com>
2024-03-29 22:35:37 +0100

    Fixing some Bugs

--- www/src/js/chat.js
+++ www/src/js/chat.js
@@ -6,15 +6,10 @@ chaticon.addEventListener('click', (event) => {
     chatbox.classList.toggle("open");
     event.preventDefault();
 });
-
-/*sendicon.addEventListener('click', () => {
-    var messageTextArea = document.getElementById('message');
-    console.log("Message Sent");
-    messageTextArea.value = '';
-});*/
-
 document.addEventListener('click', (event) => {
     if (!chatbox.contains(event.target) && event.target !== chaticon) {
         chatbox.classList.remove("open");
     }
 });
+
+
